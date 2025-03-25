9f162e7c3b01f86194731400c44cfde0bfa9ce92
SantySaar <alamedasantiago@gmail.com>
2025-03-25 21:34:23 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -3,8 +3,8 @@
 # annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
-# Additional Authors:
-# [TuNombreDeGitHub]  # ¡Reemplázalo por tu usuario!
+# Additional Authors: Santiago Alameda Recuero
+# [TuNombreDeGitHub]  #SantySaar
 
 # Input:
 # p, principal amount
